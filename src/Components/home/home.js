c6441a3091e4114e156e847ea3927736b4110727
David Biles <dlbiles1994@gmail.com>
2019-03-08 21:52:27 -0600

    small changes to home, incomplete

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 import './home.scss';
 import jaye from '../../assets/images/jaye.JPG';
+import music from '../../assets/images/music-sheet.jpg';
 
 const home = () => {
     return (
-      <>
+      <Main>
         <div className="home">
           <img src={jaye} alt="Profile pic" />
         </div>
-      </>
+      </Main>
     );
   }
 
 export default home;
+
+const Main = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vw;
+  width: 100vw;
+  background: url(${music}) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  z-index: -1;
+  opacity: 0.7;
+
+  .home {
+  position: relative;
+  margin: auto;
+  margin-top: 10vh;
+  min-height: 90vh;
+  width: 90vw;
+
+    img {
+      height: 80vh; 
+    }
+
+    .staff {
+      height: 60vh;
+      position: absolute;
+      margin: auto;
+      left: 0;
+      right: 0;
+      z-index: -1;
+    }
+  }
+`
